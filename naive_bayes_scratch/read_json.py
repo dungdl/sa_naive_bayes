1119f8f0016608909699bfdd3a5b21ad2a3c476e
dungdl28d11 <phongnt98@gmail.com>
2019-11-10 16:54:13 +0700

    test keys

--- naive_bayes_scratch/read_json.py
+++ naive_bayes_scratch/read_json.py
@@ -5,7 +5,11 @@ import json
 with open('test.json', encoding='utf8') as json_file:
     reviews = json.load(json_file)
     comments = []
+    i = 0
     for rev in reviews:
-        comments.append(rev['comment'])
+        print(rev)
+        i += 1
+        if i == 3 : 
+            break
     
         
