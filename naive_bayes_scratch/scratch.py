32c83c29951ce5da8defe001c7de468b831c518c
dungdl28d11 <phongnt98@gmail.com>
2019-11-14 16:54:29 +0700

    test vietnamese cleaner

--- naive_bayes_scratch/scratch.py
+++ naive_bayes_scratch/scratch.py
@@ -1,19 +1,44 @@
-# MARK:- Libs
-import pandas as pd
 import numpy as np
-from collections import defaultdict
+import json
+import time
 import re
 
-# MARK:- Preprocessing data
+
+# MARK:- Get content from json data file
+def readfile(filename):
+    with open(filename, encoding='utf8') as json_file:
+        reviews = json.load(json_file)
+        comments = []
+        labels = []
+        tags = []
+        for rev in reviews:
+            comments.append(rev['comment'])
+            tags.append(rev['tags'])
+    return comments
 
 
 def preprocessing_string(str):
+    # get all words from input string as space is the delimiter
+    words = re.split(' ', str)
 
-    # replace every characters except alphabet with a space
-    cleaned_str = re.sub('[^a-z\s]+', ' ', str, flags=re.IGNORECASE)
-    # merge multiple space into one
-    cleaned_str = re.sub('(\s+)', ' ', cleaned_str)
-    # lower case all characters
-    cleaned_str = cleaned_str.lower()
+    # regex to detect special characters:
+    special_regex = re.compile('[-@_!#$%^&*()<>?/\|}{~:]')
+    for word in words:
+        if special_regex.search(word):
+            if re.match(r'\w', word):
+                word = re.sub('[^a-z]', ' ', str, flags=re.IGNORECASE)
+            else:
+                words.remove(word)
+    # clean string:
+    cleaned_str = ' '.join(word for word in words)
 
     return cleaned_str
+
+
+# MARK:- start training data
+train_data = readfile('train.json')
+
+for i in range(1, 4):
+    # print(train_data[i])
+    print(preprocessing_string(train_data[i]))
+    print("----------------")
