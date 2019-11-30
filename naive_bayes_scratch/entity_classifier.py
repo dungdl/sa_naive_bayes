9c53695a1d3122af9d7f291675fffa1cc0460bf0
Nguyen Viet Hung <nviethung812@gmail.com>
2019-11-30 16:11:04 +0700

    Visualize labeling

--- naive_bayes_scratch/entity_classifier.py
+++ naive_bayes_scratch/entity_classifier.py
@@ -1,12 +1,13 @@
 # MARK:- Libs
 import sys
-sys.path.append("//Users//apple//Desktop//Sentiment Analysis//sa_naive_bayes")
+sys.path.append("..")
 from regex.read_restaurant import DataPreprocess
 from NaiveBayes import NaiveBayes
 import re
 import time
 import json
 import numpy as np
+import collections, pprint
 
 
 # MARK:- support function
@@ -14,30 +15,30 @@ import numpy as np
 # Labeling
 def labeling_entity(entity):
     switcher = {
-            "RESTAURANT" : 0,
-            "FOOD" : 1,
-            "DRINKS" : 2,
-            "AMBIENCE" : 3,
-            "SERVICE" : 4,
-            "LOCATION" : 5
+            "RESTAURANT" : 1,
+            "FOOD" : 2,
+            "DRINKS" : 3,
+            "AMBIENCE" : 4,
+            "SERVICE" : 5,
+            "LOCATION" : 6
         }
     return switcher.get(entity, 1)
 
 def labeling_attribute(attribute):
     switcher = {
-            "GENERAL" : 0,
-            "PRICES" : 1,
-            "QUALITY" : 2,
-            "STYLE&OPTIONS" : 3,
-            "MISCELLANEOUS" : 4,
+            "GENERAL" : 1,
+            "PRICES" : 2,
+            "QUALITY" : 3,
+            "STYLE&OPTIONS" : 4,
+            "MISCELLANEOUS" : 5,
         }
     return switcher.get(attribute, 1)
 
 def labeling_value(value):
     switcher = {
-            "neutral" : 0,
-            "positive" : 1,
-            "negative" : 2
+            "neutral" : 1,
+            "positive" : 2,
+            "negative" : 3
         }
     return switcher.get(value, 1)
 
@@ -59,13 +60,24 @@ def readfile(filename):
         # i = 0
         for i in range(0,len(tags)):
             tag = tags[i]
-            print(indexs[i])
-            for key in tag.keys():
-                print(key)
+            # print(indexs[i])
+            for key_entity, value_entity in tag.items():
+                label = labeling_entity(key_entity) * 100
+                for key_attr, value_attr in value_entity.items():
+                    label += labeling_attribute(key_attr) * 10 + labeling_value(value_attr)
+                    # print (label)
+                    labels.append(label)
+        
+        frequency = collections.Counter(labels)
+        frequency = collections.OrderedDict(frequency.most_common())
 
-        #     i += 1
-        #     if i > 10:
-        #         break
+        # print (frequency)
+        pprint.pprint(frequency)
+
 
 
 readfile('train.json')
+
+#--------------------------------------------------#
+
+
