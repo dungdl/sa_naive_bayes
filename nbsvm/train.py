c748fad150404f5a9d89bcb87d98d0c50fb7c6b5
dungdl28d11 <phongnt98@gmail.com>
2019-12-04 08:28:32 +0700

    train NB classifier with help from SVM

---
+++ nbsvm/train.py
@@ -0,0 +1,57 @@
+# MARK:- libs
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+import re
+import string
+
+# MARK:- input
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+subm = pd.read_csv('sample_submission.csv')
+
+# MARK:- preprocessing
+re_tok = re.compile(f'([{string.punctuation}“”¨«»®´·º½¾¿¡§£₤‘’])')
+
+
+def tokenize(s): return re_tok.sub(r' \1 ', s).split()
+
+
+def get_mdl(y):
+    y = y.values
+    r = np.log(pr(1, y) / pr(0, y))
+    m = LogisticRegression(C=4, dual=True)
+    x_nb = x.multiply(r)
+    return m.fit(x_nb, y), r
+
+
+def pr(y_i, y):
+    p = x[y == y_i].sum(0)
+    return (p+1) / ((y == y_i).sum()+1)
+
+
+
+n = train.shape[0]
+vec = TfidfVectorizer(ngram_range=(1, 2), tokenizer=tokenize,
+                      min_df=3, max_df=0.9, strip_accents='unicode', use_idf=1,
+                      smooth_idf=1, sublinear_tf=1)
+
+                    
+COMMENT = 'comment_text'
+train[COMMENT].fillna("unknown", inplace=True)
+test[COMMENT].fillna("unknown", inplace=True)
+                    
+trn_term_doc = vec.fit_transform(train[COMMENT])
+test_term_doc = vec.transform(test[COMMENT])
+
+
+x = trn_term_doc
+test_x = test_term_doc
+
+preds = np.zeros((len(test), len(label_cols)))
+
+for i, j in enumerate(label_cols):
+    print('fit', j)
+    m,r = get_mdl(train[j])
+    preds[:,i] = m.predict_proba(test_x.multiply(r))[:,1]
